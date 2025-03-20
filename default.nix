b370a0e794c69611c57678bb2e54e7880a4b75cf
Tobias Paepke <tobias@paepke.net>
2025-03-20 23:56:27 +0100

    Bump nix version

--- default.nix
+++ default.nix
@@ -1,14 +1,15 @@
 let
-
   pkgs = import (builtins.fetchGit {
-    name = "nixos-23.11";
+    name = "nixos-24.11";
     url = "https://github.com/nixos/nixpkgs/";
-    ref = "refs/heads/nixos-23.11";
-    rev = "a77ab169a83a4175169d78684ddd2e54486ac651";
-    }) {};
+    ref = "refs/heads/nixos-24.11";
+    rev = "8b27c1239e5c421a2bbc2c65d52e4a6fbf2ff296";
+  }) {};
 
   drv = pkgs.haskellPackages.developPackage {
     root = ./.;
   };
 in
-if pkgs.lib.inNixShell then drv.env else drv
+  if pkgs.lib.inNixShell
+  then drv.env
+  else drv
